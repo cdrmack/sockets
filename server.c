74ff33a5c59072b64201865d24a65ee5d83e9e02
cdrmack <git.pynvt@passmail.net>
2025-02-24 18:42:25 +0100

    create and bind socket descriptor

--- server.c
+++ server.c
@@ -1,3 +1,47 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
-int main(int artgc, char *argv[]) { return EXIT_SUCCESS; }
+#define SOCKET_NAME "/tmp/foo_socket"
+
+int main(int artgc, char *argv[])
+{
+    unlink(SOCKET_NAME);
+
+    int socket_descriptor;
+    socket_descriptor = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (socket_descriptor == -1)
+    {
+        perror("socket() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    puts("socket() call succeeded");
+
+    struct sockaddr_un address;
+    memset(&address, 0, sizeof(struct sockaddr_un));
+
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, SOCKET_NAME, sizeof(address.sun_path) - 1);
+    // address.sun_len is a legacy feature
+
+    int ret;
+    ret = bind(socket_descriptor, (struct sockaddr *)&address,
+               sizeof(struct sockaddr_un));
+
+    if (ret == -1)
+    {
+        perror("bind() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    puts("bind() call succeeded");
+
+    close(socket_descriptor);
+
+    return EXIT_SUCCESS;
+}
